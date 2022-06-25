2cf9266c5b344fa4f347d6491653591bcadfe790
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-06-25 13:18:14 +0700

    crawl movie budgets

--- README.txt
+++ README.txt
@@ -1,14 +1,9 @@
-Selenium is upgraded to v4.0.0
-pip3 install -U selenium
+If you have pip installed you can install selenium like so.
+>>>pip install selenium
+or depending on your permissions:
+>>>sudo pip install selenium
+For python3:
+>>>sudo pip3 install selenium
 
-Webdriver Manager for Python is installed
-pip3 install webdriver-manager
+>>>pip install webdriver_manager
 
-pip install beautifulsoup4
-
-1. Vào trang chrome://version/ kiểm tra phiên bản chrome, tìm ở dòng đầu tiên
-	(vd: 102.0.5005.115)
-2. Vào trang https://chromedriver.chromium.org/downloads tải về đúng phiên bản, giải nén để vào thư mục mong muốn, để đường dẫn đến file đó vào dòng 11 của file crawler.py
-	(vd: self.driver = webdriver.Chrome("C://setup//chromedriver_win32//chromedriver.exe")
-3. mở Command Prompt, đến vị trí file crawler.py được lưu
-4. gõ python drawler.py
--- crawler.py
+++ crawler.py
@@ -1,59 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup as BS
-import csv
 import pandas as pd
-import io
 
-file_name1 = 'raw-data1.csv'
-writer = csv.writer(io.open(file_name1, 'w', encoding='utf-8'))
+df = []
+page = 1
 
-pages = ['', '101', '201', '301', '401', '501', '601', '701', '801', '901', 
-        '1001', '1101', '1201', '1301', '1401', '1501', '1601', '1701', '1801', '1901',
-        '2001', '2101', '2201', '2301', '2401', '2501', '2601', '2701', '2801', '2901',
-        '3001', '3101', '3201', '3301', '3401', '3501', '3601', '3701', '3801', '3901',
-        '4001', '4101', '4201', '4301', '4401', '4501', '4601', '4701', '4801', '4901',
-        '5001', '5101', '5201', '5301', '5401', '5501', '5601', '5701', '5801', '5901',
-        '6001', '6101', '6201']
+while page < 6300:
+    url = "https://www.the-numbers.com/movie/budgets/all/" + str(page)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
 
-class craigslist_crawler(object):
-    def __init__(self, page):
-        self.url = "https://www.the-numbers.com/movie/budgets/all/" + page
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        
-    def load_page(self):
-        driver = self.driver
-        driver.get(self.url)
+    html = driver.page_source
+    df += pd.read_html(html)
 
-        html = driver.page_source
-        soup = BS(html, 'html.parser')
+    driver.close()
+    page += 100
 
-        for tr in soup.find_all('tr'):
-            data = []
+final_df = pd.concat([df[0],df[1],df[2],df[3],df[4],df[5],df[6],df[7],df[8],df[9],
+    df[10],df[11],df[12],df[13],df[14],df[15],df[16],df[17],df[18],df[19],
+    df[20],df[21],df[22],df[23],df[24],df[25],df[26],df[27],df[28],df[29],
+    df[30],df[31],df[32],df[33],df[34],df[35],df[36],df[37],df[38],df[39],
+    df[40],df[41],df[42],df[43],df[44],df[45],df[46],df[47],df[48],df[49],
+    df[50],df[51],df[52],df[53],df[54],df[55],df[56],df[57],df[58],df[59],
+    df[60],df[61],df[62]])
 
-            # execute only once
-            # for th in tr.find_all('th'):
-            #     data.append(th.text)
-            # if(data):
-            #     print("Inserting header: {}".format(','.join(data)))
-            #     writer.writerow(data)
-            #     continue
-
-            for td in tr.find_all('td'):
-                data.append(td.text.strip().replace('$','').replace(',',''))
-            if(data):
-                print("Inserting row: {}".format(','.join(data)))
-                writer.writerow(data)
-        driver.close()
-
-
-for page in pages:
-    crawler = craigslist_crawler(page)
-    crawler.load_page()
-
-# delete empty rows
-file_name2 = 'raw-data.csv'
-df = pd.read_csv(file_name1, encoding='unicode_escape')
-mdf = df.dropna()
-mdf.to_csv(file_name2, index=False)
+final_df.to_csv('raw-data.csv')
